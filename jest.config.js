fc9659c9e3961f121bfcc3ed3b5eaeffda9d9dd3
Ankit Mehrotra <ankit_mehrotra85@yahoo.com>
2022-01-25 11:04:31 +0100

    added coverage

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,21 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  globals:{
-    'ts-jest':{
-      tsconfig: 'tsconfig.jest.json'
-    }
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+    },
   },
-  setupFilesAfterEnv: ['./src/jest.setup.ts']
+  setupFilesAfterEnv: ["./src/jest.setup.ts"],
+  jest: {
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: -10,
+      },
+    },
+  },
 };
