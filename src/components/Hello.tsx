d7d0d08671015450a990a1e4d4076e9e04ab9769
Ankit Mehrotra <ankit_mehrotra85@yahoo.com>
2022-01-25 10:42:30 +0100

    eslint testing

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,3 +1,8 @@
-export function Hello(){
-    return <div>Hello World!!</div>
+export function Hello() {
+  return (
+    <div>
+      Hello World!!
+      <h3>Hey there !!</h3>
+    </div>
+  );
 }
